98799745d59185b104d8ae0aa291ef2ef1bed3a9
VincciLee <vinx1ce@gmail.com>
2022-05-22 12:57:17 +0800

    Done Sorting and half Delete
    - Delete left check terminated date
    - Modify need wait view Tutor Details

--- Tutor_Management_System/Tutor_Management_System_Array/Tutor.h
+++ Tutor_Management_System/Tutor_Management_System_Array/Tutor.h
@@ -26,16 +26,19 @@ public: //Constructor
 
 void test(string test);  //Sample Function
 
+void definedTutor(Tutor* head);	// Predefined Tutors
+
 void mainMenu(); // Main Menu
-void tutorListMenu(); // Main Menu > Display All Tutor
-void tutorMenuControl(int* input); //Tutor List Menu Control
+void tutorListMenu(Tutor* ptr, int* size, int* currentPage); // Main Menu > Display All Tutor
+void tutorMenuControl(int* input, Tutor* head, int* size, int* currentPage); //Tutor List Menu Control
 
 void addTutor(Tutor* head, Tutor* newData); //Adds a new tutor to the list
 //void modifyTutor(Tutor* head, string tutorId, string data, string attribute); //Updates the tutor record
-//bool deleteTutor(Tutor* head, string tutorId); //Deletes the selected tutor and returns True/False to indicate success
-//void displayTutorList(Tutor* head) {} //Displays the list of all tutors
-//void sortByTutorId(Tutor** head){} //Sort the tutor list by tutor id
-//void sortByHourlyPay(Tutor** head) {} //Sort the tutor list by hourly pay
-//void sortByRating(Tutor** head) {} //Sort the tutor list by rating
+bool deleteTutor(Tutor* head, int low, int size, int tutorId); //Deletes the selected tutor and returns True/False to indicate success
+void displayTutorList(Tutor* head, int size, int* currentPage); //Displays the list of all tutors
+void sortByTutorId(Tutor* head, int size); //Sort the tutor list by tutor id
+void sortByHourlyPay(Tutor* head, int low, int high); //Sort the tutor list by hourly pay
+void sortByRating(Tutor* head, int low, int high); //Sort the tutor list by rating
+void mergeSorting(Tutor* head, int low, int high, int mid); // MergeSort function for sortByRating
 //Tutor* searchByTutorId(Tutor* head, int tutorId) {} //Search for a tutor by tutor id
 //Tutor* searchhByRating(Tutor* head, int rating) {} //Search for a tutor by rating
