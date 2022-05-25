e3aa2163d130b103d4bbc68e528f315a230af680
VincciLee <vinx1ce@gmail.com>
2022-05-25 15:17:36 +0800

    Complete up to modify
    - Optimize display all interface
    - Done Display single data and paging
    - Complete modify

--- Tutor_Management_System/Tutor_Management_System_Array/Tutor.h
+++ Tutor_Management_System/Tutor_Management_System_Array/Tutor.h
@@ -3,8 +3,14 @@
 #include <string>
 #include <ctime>
 #include <chrono>
+#include<iostream>
+#include<fstream> //header file used for file i/o
+#include<iomanip>
+
 using namespace std;
-	struct Tutor { //Implementation of Tutor for array
+using namespace std::chrono;
+
+struct Tutor { //Implementation of Tutor for array
 
 	//Data Fields
 	int tutorId;
@@ -19,27 +25,46 @@ using namespace std;
 	int subjectCode;
 	string subjectName;
 	int rating;
-
-public: //Constructor
-	Tutor();
-	Tutor(int tutorId); //Sample of custom constructor
 };
 
-void test(string test);  //Sample Function
-
+/*
+===========================================================
+GENERAL FUNCTIONS
+===========================================================
+*/
 void definedTutor(Tutor* head);	// Predefined Tutors
-
+bool login(string *userRole);
 void mainMenu(); // Main Menu
-void tutorListMenu(Tutor* ptr, int* size, int* currentPage); // Main Menu > Display All Tutor
-void tutorMenuControl(int* input, Tutor* head, int* size, int* currentPage); //Tutor List Menu Control
+void tutorListMenu(string *userRole, Tutor* ptr, int* size, int* currentPage); // Main Menu > Display All Tutor
+void tutorListMenuControl(int* input, Tutor* head, int* size, int* currentPage, string *userRole); //Tutor List Menu Control
+void tutorMenuOptions(string *userRole);
+int tutorMenuControl(Tutor *head, int size, int menuSelection, int tutorId);
+void logToFile(string data);
+
+Tutor *displayTutorDetails(Tutor *head, int size, int tutorId);
+void displayTutorList(Tutor* head, int size, int* currentPage); //Displays the list of all tutors
 
 void addTutor(Tutor* head, Tutor* newData); //Adds a new tutor to the list
-bool modifyTutor(Tutor* head, int low, int size, int tutorId); //Updates the tutor record
+bool modifyTutor(Tutor *data, string *dataValue, string updateAttribute); //Updates the tutor record
 bool deleteTutor(Tutor* head, int low, int size, int tutorId); //Deletes the selected tutor and returns True/False to indicate success
-void displayTutorList(Tutor* head, int size, int* currentPage); //Displays the list of all tutors
+
+/*
+===========================================================
+SORTING FUNCTIONS
+===========================================================
+*/
 void sortByTutorId(Tutor* head, int size); //Sort the tutor list by tutor id
 void sortByHourlyPay(Tutor* head, int low, int high); //Sort the tutor list by hourly pay
 void sortByRating(Tutor* head, int low, int high); //Sort the tutor list by rating
 void mergeSorting(Tutor* head, int low, int high, int mid); // MergeSort function for sortByRating
+
+/*
+===========================================================
+SEARCHING FUNCTIONS
+===========================================================
+*/
 //Tutor* searchByTutorId(Tutor* head, int tutorId) {} //Search for a tutor by tutor id
 //Tutor* searchhByRating(Tutor* head, int rating) {} //Search for a tutor by rating
+
+Tutor *binarySearchTutorId(Tutor* head, int size, int tutorId);
+int binarySearchTutorIndex(Tutor* head, int size, int tutorId);
