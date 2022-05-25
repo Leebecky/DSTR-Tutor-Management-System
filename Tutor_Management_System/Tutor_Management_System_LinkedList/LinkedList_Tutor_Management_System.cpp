101d5818f2cdb554bf8fb02341b1d378914ee0f9
Leebecky <leebecky06@gmail.com>
2022-05-25 13:59:21 +0800

    Code Tidying
    - tidied up Tutor.h in Linked List
    - fixed display elements in Linked List
    - fixed individual pagination

--- Tutor_Management_System/Tutor_Management_System_LinkedList/LinkedList_Tutor_Management_System.cpp
+++ Tutor_Management_System/Tutor_Management_System_LinkedList/LinkedList_Tutor_Management_System.cpp
@@ -21,16 +21,16 @@ int main()
 	int tutorListCount = 0;
 	int currentPage = 1;
 
-	string userRole = "Admin";
+	string userRole = "HR";
 	bool auth = false;
 
 	cout << "Tutor Linked List System: " << timeStamp << endl;
 	cout << "Please login..." << endl;
-	cout << "Login is currently disabled...";
+	cout << "Login is currently disabled..." <<endl;
 	/*do {
 		auth = login(&userRole);
-	} while (!auth);*/
-
+	} while (!auth);
+*/
 	cout << "Welcome, " << userRole << ". Hit Enter to proceed" << endl;
 
 	cin.get();
@@ -71,7 +71,7 @@ int main()
 					goto resetDisplayMenu;
 				}
 
-				tutorMenuControl(&tutorMenu, &head, &tail, &tutorListCount, &currentPage, &userRole);
+				tutorListMenuControl(&tutorMenu, &head, &tail, &tutorListCount, &currentPage, &userRole);
 
 
 
--- Tutor_Management_System/Tutor_Management_System_LinkedList/Tutor.h
+++ Tutor_Management_System/Tutor_Management_System_LinkedList/Tutor.h
@@ -40,24 +40,41 @@ public: //Constructor
 GENERAL FUNCTIONS
 ===========================================================
 */
-bool login(string *userRole);
+bool login(string *userRole); // User login authentication
+void logToFile(string data); // log time taken to file
+void generateData(Tutor **head, Tutor** tail, int *tutorListCount); // Contains the predefined data
+
+
+/*
+===========================================================
+MENU DISPLAY & CONTROL FUNCTIONS
+===========================================================
+*/
+
 void mainMenu(); // Main Menu
 void tutorListMenu(string *userRole); // Main Menu > Display All Tutor
-void tutorMenuControl(int* input, Tutor** head, Tutor** tail, int *count, int* currentPage, string *userRole); //Tutor List Menu Control
-void generateData(Tutor **head, Tutor** tail, int *tutorListCount); // Contains the predefined data
-void logToFile(string data); // log time taken to file
-Tutor* findMid(Tutor *head, int index, int mid);
-
+void tutorListMenuControl(int* input, Tutor** head, Tutor** tail, int *count, int* currentPage, string *userRole); //Tutor List Menu Control
 void tutorMenuOptions(string *userRole);
-int tutorMenuControl(Tutor *head, int size, int menuSelection, int tutorId);
+int tutorMenuControl(Tutor *head, Tutor *tail, int size, int menuSelection, int tutorId);
+
+/*
+===========================================================
+DATA DISPLAY FUNCTIONS
+===========================================================
+*/
 
 void displayTutorList(Tutor* head, int size, int* currentPage); //Displays the list of all tutors
 Tutor *displayTutorDetails(Tutor *head, int size, int tutorId); //Displays the details of a tutor
 
+/*
+===========================================================
+DATA CONTROL FUNCTIONS
+===========================================================
+*/
+
 //void addTutor(Tutor* head, Tutor* newData); //Adds a new tutor to the list
 bool modifyTutor(Tutor *data, string *dataValue, string updateAttribute); //Updates the tutor record
 bool deleteTutor(Tutor** head, Tutor  **tail, int size, int tutorId); //Deletes the selected tutor and returns True/False to indicate success
-
 
 
 /*
@@ -66,12 +83,16 @@ SORTING FUNCTIONS
 ===========================================================
 */
 void sortByTutorId(Tutor **head, Tutor **tail, int *count); //Sort the tutor list by tutor id
-Tutor *getTail(Tutor *cur);
-Tutor* swapNode(Tutor* node1, Tutor* node2);
 void sortByHourlyPay(Tutor** head, Tutor **tail); //Sort the tutor list by hourly pay
 void sortByRating(Tutor** head, Tutor **tail); //Sort the tutor list by rating
+
+// ==== Utility Sorting Functions  //
+Tutor *getTail(Tutor *cur);
+Tutor* swapNode(Tutor* node1, Tutor* node2);
 void MergeSort(Tutor** head); //MergeSort the tutor list by rating
 Tutor *quickSortRecur(Tutor *head, Tutor *tail);
+Tutor* findMid(Tutor *head, int index, int mid);
+
 /*
 ===========================================================
 SEARCHING FUNCTIONS
